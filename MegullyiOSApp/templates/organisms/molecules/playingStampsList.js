ac6da9ff98ba3559075387c139847d61fe04ce78
peachparfait <peachparfait10@gmail.com>
2021-05-23 20:10:07 +0900

    スタンプ一覧画面を追加

--- MegullyiOSApp/templates/organisms/molecules/playingStampsList.js
+++ MegullyiOSApp/templates/organisms/molecules/playingStampsList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 import { StyleSheet, Text, View ,Image, FlatList,SafeAreaView} from 'react-native';
 import {Dimensions} from 'react-native';
 import Colors from '../../../assets/colors.json';
@@ -11,12 +12,18 @@ const wid = Dimensions.get('screen').width;
 const hei = Dimensions.get('screen').height;
 import ContentLoader, { Facebook } from 'react-content-loader/native'
 
-
+function StampedChecker(props){
+  if(props.stamped == 1){
+    return(<PlaceImg img={props.img}/>);
+  }else{
+    return(<Icon name='map-marker-alt' size={70} color={Colors.colors.gray} style={styles.icon}/>);
+  }
+}
 export default function PlayingStampsList(props) {
 return (
   <View style={styles.container}>
     <View style={styles.titles}>
-      <PlaceImg img={props.img}/>
+      <StampedChecker stamped={props.stamped} img={props.img}/>
       <View style={styles.aboutPlace}>
         <PlaceNameBody name={props.name}/>
       </View>
@@ -49,4 +56,8 @@ aboutPlace:{
 dirs:{
   flex:1,
 },
+icon:{
+  marginLeft:23,
+  marginRight:24,
+}
 });
--- MegullyiOSApp/templates/organisms/playingStampsModal.js
+++ MegullyiOSApp/templates/organisms/playingStampsModal.js
@@ -2,19 +2,20 @@ import React from 'react';
 import { StyleSheet, Text, View,Modal,Pressable,FlatList} from 'react-native';
 import {Dimensions} from 'react-native';
 import PlayingStampsList from "./molecules/playingStampsList";
+import Icon from 'react-native-vector-icons/FontAwesome5';
 const wid = Dimensions.get('screen').width;
 const hei = Dimensions.get('screen').height;
 import Colors from '../../assets/colors.json';
 
 const stampsList=[
-  {id:'0',name:"東京スカイツリー",stamped:true,img:require('../../assets/img/tokyo.jpg')},
+  {id:'0',name:"東京スカイツリー",stamped:true,img:require('../../assets/img/skyTree.jpg')},
   {id:'1',name:"東京駅",stamped:false,img:require('../../assets/img/tokyoStation.jpg')},
-  {id:'2',name:"東京タワー",stamped:true,img:require('../../assets/img/dousojin.jpg')},
-  {id:'3',name:"東京ビッグサイト",stamped:false,img:require('../../assets/img/shikoku.png')},
-  {id:'4',name:"新宿御苑",stamped:true,img:require('../../assets/img/meijiJingu.jpg')},
-  {id:'5',name:"コクーンタワー",stamped:true,img:require('../../assets/img/arrowSign.png')},
-  {id:'6',name:"レインボーブリッジ",stamped:false,img:require('../../assets/img/superSento.png')},
-  {id:'7',name:"原宿",stamped:false,img:require('../../assets/img/gojo.png')},
+  {id:'2',name:"東京タワー",stamped:true,img:require('../../assets/img/tokyoTower.jpg')},
+  {id:'3',name:"東京ビッグサイト",stamped:false,img:require('../../assets/img/bigSite.jpg')},
+  {id:'4',name:"新宿御苑",stamped:true,img:require('../../assets/img/shinjukuGarden.jpg')},
+  {id:'5',name:"浅草",stamped:true,img:require('../../assets/img/kaminarimon.jpg')},
+  {id:'6',name:"レインボーブリッジ",stamped:false,img:require('../../assets/img/rainbowBridge.jpg')},
+  {id:'7',name:"コクーンタワー",stamped:false,img:require('../../assets/img/cocoonTower.jpg')},
   ];
 const Item = ({name,stamped,img}) => (
   <View>
@@ -29,16 +30,13 @@ export default function PlayingStampsModal(props) {
     <View style={styles.container}>
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
-            <Pressable
-              style={[styles.button, styles.buttonClose]}
-              onPress={props.onPress}
-            >
-              <Text style={styles.textStyle}>Hide Modal</Text>
-            </Pressable>
+            <Icon.Button name='times' size={20} color={Colors.colors.text} backgroundColor="#fff" style={styles.icon} onPress={props.onPress} iconStyle={{marginLeft: 20,marginTop:50,}}/>
+            <Text style={styles.text}>東京観光ツアー</Text>
             <FlatList
             data={stampsList}
             renderItem={renderItem}
             keyExtractor={item => item.id}
+            style={styles.list}
             />
           </View>
         </View>
@@ -55,11 +53,10 @@ const styles = StyleSheet.create({
     marginTop: 22,
   },
   modalView: {
-    marginTop: 10,
+    marginTop: 15,
     backgroundColor:Colors.colors.background,
-    paddingTop: 35,
+    paddingTop: 25,
     height:hei -30,
-    alignItems: "center",
     shadowColor: Colors.colors.shadow,
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
@@ -70,17 +67,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 4,
     elevation: 1,
-  },
-  button: {
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-  },
-  buttonOpen: {
-    backgroundColor: "#F194FF",
-  },
-  buttonClose: {
-    backgroundColor: "#2196F3",
+    alignItems:'center',
   },
   textStyle: {
     color: "white",
@@ -90,5 +77,25 @@ const styles = StyleSheet.create({
   modalText: {
     marginBottom: 15,
     textAlign: "center",
+  },
+  icon:{
+    position:'relative',
+    left:300,
+    height:100,
+    width:100,
+    marginRight:wid/2+90,
+    marginTop:-52,
+    alignItems: 'center',
+  },
+  text: {
+    fontSize:20,
+    fontWeight:"800",
+    color:Colors.colors.text,
+    marginLeft:2,
+    marginBottom:30,
+    marginTop:-35,
+  },
+  list:{
+    zIndex:1,
   }
 });
